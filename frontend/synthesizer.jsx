14e893bc6f3e7782ecb27581cf12d8e4366b7868
David Hu <davidhu314@gmail.com>
2016-12-26 15:45:47 -0800

    add app.jsx and used provider  ot wrap app class

--- frontend/synthesizer.jsx
+++ frontend/synthesizer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store.js';
+import Root from './components/root.jsx';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Synthesizer</h1>, root);
-  window.store = store;
+  ReactDOM.render(<Root store={store} />, root);
 });
